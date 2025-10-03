ce55a7c5468de3f5f54b435aa9e8c0d66f1604b9
mmanneyy <mmanneyy333@gmail.com>
2025-10-03 22:48:37 +0400

    file

--- Algoritms/LinkedList/singlyLinkedList.js
+++ Algoritms/LinkedList/singlyLinkedList.js
@@ -1,4 +1,4 @@
-//NEED TO FINISH
+//NEED TO FINISH (remove)
 class Node {
     constructor(data, next = null) {
         this.data = data;
@@ -81,7 +81,6 @@ class SinglyLinkedList {
     }
 
     pop_back() {
-        let res = 0;
         if(!this.#head) return undefined;
         if(this.#size === 1) {
             res = this.#head;
@@ -93,22 +92,174 @@ class SinglyLinkedList {
 
         let headCpy = this.#head;
 
-        while(headCpy) { 
-            if(headCpy.next === this.#tail) {
-                let res = this.#tail.data;
-                this.#tail = headCpy;
-                headCpy.next = null;   
-                this.#tail.next = null;
-            }
+        
+        while(headCpy.next !== this.#tail) {
+            headCpy = headCpy.next;
         }
+        let res = this.#tail.data;
+        this.#tail = headCpy;
+        headCpy.next = null;   
+        this.#tail.next = null;
+        this.#size--;
         return res;
     }
 
     pop_front() {
+        if(!this.#head) return null;
+        const n = this.#head.data;
+        if(this.#size > 1) {
+            this.#head = this.#head.next;
+        } else {
+            this.#head = this.#tail = null;
+        }
+        this.#size--;
+        return n;
+    }
+
+    //RANDOM-LIKE OPERATIONS O(n)
+    at(idx) {
+        let counter = 0;
+        let headCpy = this.#head;
+        while(headCpy) {
+            if(counter === idx) return headCpy.data;
+            headCpy = headCpy.next;
+            counter++
+        }
+        return null;
+    }
+
+    insert(idx, value) {
+        let counter = 0;
+        let headCpy = this.#head;
+        let newN = new Node();
+        newN.data = value;
+        newN.next = null;
+        if(idx === 0) {
+            this.#head = newN;
+            this.#head.next = headCpy;
+            this.#size++;
+            return;
+        } 
+        if(idx === this.#size) {
+            let tailCpy = this.#tail;
+            tailCpy.next = newN;
+            this.#tail = newN;
+            this.#tail.next = null;
+            this.#size++;
+            return;
+        }
+        while(headCpy) {
+            if(idx - counter === 1) {
+                newN.next = headCpy.next;
+                headCpy.next = newN;
+                this.#size++;
+                return;
+            }
+            counter++;
+            headCpy = headCpy.next;
+        }
+    }
+
+    erase(idx) {
+        if(!this.#head) return null;
+        if(idx === 0) {
+            let rmvd = this.#head.data;
+            this.#head = this.#head.next;
+            if(!this.#head) this.#tail = null;
+            this.#size--;
+            return rmvd;
+        }
+        let headCpy = this.#head;
+        let counter = 0;
+        while(headCpy) {
+            if(idx - counter === 1) {
+                let rmvd = headCpy.next.data;
+                headCpy.next = headCpy.next.next;
+                if(!headCpy.next) this.#tail = headCpy;
+                this.#size--;
+                return rmvd;
+            }
+            headCpy = headCpy.next;
+            counter++;
+        }
+        return undefined;
+    }
+
+    remove(value, equals) {
+        let headCpy = this.#head;
+        let prev = null;
+        let curr = this.#head;
+        while(curr) {
+            if(equals) {
+                if(equals(curr.data, value)) {
+                    prev.next = curr.next;
+                } else {
+                    if(value === curr.data) {
+                        prev.next = curr.next;
+                    }
+                }
+            }
+            prev = curr;
+            curr = curr.next;
+            this.#size--;
+        }
+        return;
+    }
+
+    //ALGORITHMS
+
+    reverse() {
+        if(this.#size <= 1) return;
 
     }
 
+    merge(left, right) {
+        if(!left) return right;
+        if(!right) return   
+        let dummy = new Node(null);
+        let curr = dummy;
 
+        while(left && right) {
+            if(left.data <= right.data) {
+                curr.next = left;
+                left = left.next;
+            } else {
+                curr.next = right;
+                right = right.next;
+            }
+        }
+
+        if(left) {
+            curr.next = left;
+        }
+        if(right) {
+            curr.next = right;
+        }
+        return dummy.next;
+    }
+
+    mergeSort(head) {
+        if(!head || !head.next) return head;
+        let fast = head.next;
+        let slow = head;
+
+        while(fast && fast.next) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        let mid = slow.next;
+        slow.next = null;
+
+        const left = this.mergeSort(head);
+        const right = this.mergeSort(mid);
+
+        return this.merge(left, right)
+    }
+    sort() {
+        if(this.#size < 2) return;
+        this.#head = this.mergeSort(this.#head);
+    }
 }
 
 
